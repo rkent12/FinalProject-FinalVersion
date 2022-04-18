082c1e0c8a7365eb9b25ed68b5d9ba4b1097d7bb
rkent12 <rkent@mail.endicott.edu>
2022-04-17 21:20:47 -0400

    Newest Push because I didn't realize something failed in the last one, so this adds all the dimension and block UI stuff, as well as the block quest code as well.

---
+++ src/main/java/net/ryankent/practicemod/world/gen/ModWorldGen.java
@@ -0,0 +1,11 @@
+package net.ryankent.practicemod.world.gen;
+
+
+public class ModWorldGen {
+    public static void generateModWorldGen() {
+        ModOreGeneration.generateOres();
+        ModTreeGeneration.generateTrees();
+    }
+
+
+}
